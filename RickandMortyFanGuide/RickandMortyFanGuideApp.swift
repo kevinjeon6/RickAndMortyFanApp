10121389f17dbab6f832655a208f21f671ee79cd
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-06-17 11:16:32 -0500

    Initial Commit

---
+++ RickandMortyFanGuide/RickandMortyFanGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickandMortyFanGuideApp.swift
+//  RickandMortyFanGuide
+//
+//  Created by Kevin Mattocks on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct RickandMortyFanGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
