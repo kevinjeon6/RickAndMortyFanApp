a5d3114dbff9a5a84e7e2e8fd2c43ef95b8e61bf
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-07-26 14:11:15 -0400

    Change Favorites to SaveFavorites for key Add JSONDecoder logic to init() Update parameter type in Favorites Add JSONEncoder logic to save function in Favorites

--- RickandMortyFanGuide/Components/Favorites.swift
+++ RickandMortyFanGuide/Components/Favorites.swift
@@ -9,27 +9,35 @@ import Foundation
 
 class Favorites: ObservableObject {
     private var characters: Set<String>
-    
-    private let saveKey = "Favorites"
+
+    private let saveKey = "SaveFavorites"
     
     init(){
         //load saved data
-        
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
+
+            
+            //Using Set<String> because we don't want to store the entire struct, just the id
+            if let decoded = try? JSONDecoder().decode(Set<String>.self, from: data) {
+                characters = decoded
+                return
+            }
+        }
         characters = []
     }
     
     
-    func contains(_ character: CharacterResults) -> Bool {
+    func contains(_ character: Character) -> Bool {
         characters.contains(String(character.id))
     }
     
-    func add(_ character: CharacterResults) {
+    func add(_ character: Character) {
         objectWillChange.send()
         characters.insert(String(character.id))
         save()
     }
     
-    func remove(_ character: CharacterResults) {
+    func remove(_ character: Character) {
         objectWillChange.send()
         characters.remove(String(character.id))
         save()
@@ -37,6 +45,8 @@ class Favorites: ObservableObject {
     
     
     func save(){
-        
+        if let encoded = try? JSONEncoder().encode(characters) {
+            UserDefaults.standard.set(encoded, forKey: saveKey)
+        }
     }
 }
